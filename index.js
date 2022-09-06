b125f0edc4bcad4c89d2859858d9507937c12d8d
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-09-06 20:26:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let str = word.split("").reverse().join("");
+  const checkStr = str === word ? true : false ;
+  return checkStr ;
 }
 
 /* 
   Add your pseudocode here
+  str = variable to store the reversed arguement
+  checkStr = checks if the passed arguement is a palindrome
 */
 
 /*
   Add written explanation of your solution here
+  the string passed as an arguement is split,reversed and joined to form a new string and stored in the variable str
+  const checkStr tests if it a palindrome and returns the value found
 */
 
 // You can run `node index.js` to view these console logs
